d4119b208144352385f31751fa9fb8de6fd3b595
zhangqqing <1794541452@qq.com>
2022-06-02 20:58:45 +0800

    first commit

---
+++ src/components/Logout.vue
@@ -0,0 +1,129 @@
+<template>
+  <div class="logout">
+    <el-form ref="userFormRef" :model="userForm" :rules="userFormRules" label-width="0px" class="user_form">
+      <!-- 用户id -->
+      <el-form-item prop="id">
+        <el-input
+            v-model="userForm.id" placeholder="id"
+        >
+        </el-input>
+      </el-form-item>
+      <!-- token -->
+      <el-form-item prop="token">
+        <el-input
+            v-model="userForm.token" type="token" placeholder="token"
+        >
+        </el-input>
+      </el-form-item>
+      <!-- 按钮区域 -->
+      <el-row justify="center">
+        <el-form-item class="user_btn">
+          <el-button type="primary" @click="logout()">登出</el-button>
+          <el-button type="info" @click="resetUserForm">重置</el-button>
+        </el-form-item>
+      </el-row>
+    </el-form>
+  </div>
+  <div class="msg">
+    <p>{{msg}}</p>
+  </div>
+</template>
+
+<script>
+const axios=require('axios').default
+export default {
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: "Logout",
+  data(){
+    return {
+      // 这是表单的数据绑定对象
+      userForm: {
+        id: '',
+        token: ''
+      },
+      // 这是表单的验证规则对象
+      userFormRules: {
+        id: [
+          {required: true, message: '不能为空', trigger: 'blur'},
+          {min: 1, max: 10, message: '长度在 1 到 10 个字符', trigger: 'blur'}
+        ],
+        token: [
+          {required: true, message: '不能为空', trigger: 'blur'},
+          {min: 32, max: 32, message: '长度 32 个字符', trigger: 'blur'}
+        ]
+      },
+      msg: "",
+    }
+  },
+  methods:{
+    resetUserForm() {
+      this.$refs.userFormRef.resetFields()
+    },
+    logout(){
+      this.$refs.userFormRef.validate(async valid => {
+        if (!valid) return
+        axios.put('/api/start/logout/',this.userForm).then((res)=>{
+          this.id=res.data.id
+          this.token=res.data.token
+          this.msg="Successfully logged out!"
+        }).catch((error)=>{
+          this.msg='error'
+          console.log(error);
+        })
+     })
+   }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.logout {
+  // 宽450像素
+  width: 450px;
+  // 高300像素
+  height: 200px;
+  // 边框线1像素 实线
+  border: 1px solid grey;
+  // 圆角边框3像素
+  border-radius: 3px;
+  // 绝对定位
+  position: absolute;
+  // 距离左则50%
+  left: 50%;
+  // 上面距离30%
+  top: 30%;
+  // 进行位移，并且在横轴上位移-50%，纵轴也位移-50%
+  transform: translate(-50%, -50%);
+}
+.user_form {
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  padding: 0 20px;
+  box-sizing: border-box;
+}
+.user_btn {
+  // 设置弹性布局
+  display: flex;
+  // 横轴上尾部对齐
+  justify-content: flex-start;
+}
+.msg {
+  // 宽450像素
+  width: 450px;
+  // 高300像素
+  height: 100px;
+  // 边框线1像素 实线
+  border: 1px solid grey;
+  // 圆角边框3像素
+  border-radius: 3px;
+  // 绝对定位
+  position: absolute;
+  // 距离左则50%
+  left: 50%;
+  // 上面距离30%
+  top: 60%;
+  // 进行位移，并且在横轴上位移-50%，纵轴也位移-50%
+  transform: translate(-50%, -50%);
+}
+</style>.
---
+++ src/components/Register.vue
@@ -0,0 +1,132 @@
+<template>
+  <div class="register">
+    <el-form ref="userFormRef" :model="userForm" :rules="userFormRules" label-width="0px" class="user_form">
+      <!-- 用户名 -->
+      <el-form-item prop="username">
+        <el-input
+            v-model="userForm.username" placeholder="username"
+        >
+        </el-input>
+      </el-form-item>
+      <!-- 密码 -->
+      <el-form-item prop="password">
+        <el-input
+            v-model="userForm.password" type="password" placeholder="password" show-password
+        >
+        </el-input>
+      </el-form-item>
+      <!-- 按钮区域 -->
+      <el-row justify="center">
+        <el-form-item class="user_btn">
+          <el-button type="primary" @click="register()">注册</el-button>
+          <el-button type="info" @click="resetUserForm">重置</el-button>
+        </el-form-item>
+      </el-row>
+    </el-form>
+  </div>
+  <div class="msg">
+    <p>{{msg}}</p>
+  </div>
+
+</template>
+
+<script>
+  const axios=require('axios').default
+  export default {
+    // eslint-disable-next-line vue/multi-word-component-names
+    name: "Register",
+    data() {
+      return {
+        // 这是表单的数据绑定对象
+        userForm: {
+          username: '',
+          password: ''
+        },
+        // 这是表单的验证规则对象
+        userFormRules: {
+          // 验证用户名是否合法
+          username: [
+            {required: true, message: '不能为空', trigger: 'blur'},
+            {min: 3, max: 10, message: '长度在 3 到 10 个字符', trigger: 'blur'}
+          ],
+          // 验证密码是否合法
+          password: [
+            {required: true, message: '不能为空', trigger: 'blur'},
+            {min: 6, max: 15, message: '长度在 6 到 15 个字符', trigger: 'blur'}
+          ]
+        },
+        msg: "",
+      }
+    },
+    methods: {
+      resetUserForm() {
+        this.$refs.userFormRef.resetFields()
+      },
+      register() {
+        this.$refs.userFormRef.validate(async valid => {
+          if (!valid) return
+          axios.post('/api/start/register/', this.userForm).then((res) => {
+            this.uid = res.data.id
+            this.username = res.data.username
+            this.msg = "Successfully registered!"
+          }).catch((error) => {
+            this.msg = 'error'
+            console.log(error);
+          })
+        })
+      }
+    }
+  }
+</script>
+
+<style lang="less" scoped>
+.register {
+  // 宽450像素
+  width: 450px;
+  // 高300像素
+  height: 200px;
+  // 边框线1像素 实线
+  border: 1px solid grey;
+  // 圆角边框3像素
+  border-radius: 3px;
+  // 绝对定位
+  position: absolute;
+  // 距离左则50%
+  left: 50%;
+  // 上面距离30%
+  top: 30%;
+  // 进行位移，并且在横轴上位移-50%，纵轴也位移-50%
+  transform: translate(-50%, -50%);
+}
+.user_form {
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  padding: 0 20px;
+  box-sizing: border-box;
+}
+.user_btn {
+// 设置弹性布局
+display: flex;
+// 横轴上尾部对齐
+justify-content: flex-start;
+}
+.msg {
+  // 宽450像素
+  width: 450px;
+  // 高300像素
+  height: 100px;
+  // 边框线1像素 实线
+  border: 1px solid grey;
+  // 圆角边框3像素
+  border-radius: 3px;
+  // 绝对定位
+  position: absolute;
+  // 距离左则50%
+  left: 50%;
+  // 上面距离30%
+  top: 60%;
+  // 进行位移，并且在横轴上位移-50%，纵轴也位移-50%
+  transform: translate(-50%, -50%);
+}
+</style>
